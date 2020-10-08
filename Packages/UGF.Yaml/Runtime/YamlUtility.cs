0d18b01078bf93f14d79019891255daa4ff6dc78
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-08 21:32:42 +0300

    Update YamlUtility.cs

--- Packages/UGF.Yaml/Runtime/YamlUtility.cs
+++ Packages/UGF.Yaml/Runtime/YamlUtility.cs
@@ -1,6 +1,40 @@
-﻿namespace UGF.Yaml.Runtime
+﻿using System;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace UGF.Yaml.Runtime
 {
     public static class YamlUtility
     {
+        private static readonly ISerializer m_serializer;
+        private static readonly IDeserializer m_deserializer;
+
+        static YamlUtility()
+        {
+            m_serializer = new SerializerBuilder().Build();
+            m_deserializer = new DeserializerBuilder().WithNamingConvention(new CamelCaseNamingConvention()).Build();
+        }
+
+        public static string ToYaml(object target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+
+            return m_serializer.Serialize(target);
+        }
+
+        public static object FromYaml(string text, Type type)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentException("Value cannot be null or empty.", nameof(text));
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            return m_deserializer.Deserialize(text, type);
+        }
+
+        public static T FromYaml<T>(string text)
+        {
+            if (string.IsNullOrEmpty(text)) throw new ArgumentException("Value cannot be null or empty.", nameof(text));
+
+            return m_deserializer.Deserialize<T>(text);
+        }
     }
 }
