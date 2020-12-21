4bdd45a9ed8220dff04b7f17dbe90abc033d517e
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-21 20:57:57 +0300

    Update YamlUtility.cs

--- Packages/UGF.Yaml/Runtime/YamlUtility.cs
+++ Packages/UGF.Yaml/Runtime/YamlUtility.cs
@@ -6,18 +6,21 @@ namespace UGF.Yaml.Runtime
 {
     public static class YamlUtility
     {
-        private static readonly ISerializer m_serializer;
-        private static readonly IDeserializer m_deserializer;
+        public static ISerializer DefaultSerializer { get; } = CreateDefaultSerializer();
+        public static IDeserializer DefaultDeserializer { get; } = CreateDefaultDeserializer();
 
-        static YamlUtility()
+        public static ISerializer CreateDefaultSerializer()
         {
-            m_serializer = new SerializerBuilder()
+            return new SerializerBuilder()
                 .DisableAliases()
                 .EmitDefaults()
                 .WithNamingConvention(new CamelCaseNamingConvention())
                 .Build();
+        }
 
-            m_deserializer = new DeserializerBuilder()
+        public static IDeserializer CreateDefaultDeserializer()
+        {
+            return new DeserializerBuilder()
                 .IgnoreUnmatchedProperties()
                 .WithNamingConvention(new CamelCaseNamingConvention())
                 .Build();
@@ -27,7 +30,7 @@ namespace UGF.Yaml.Runtime
         {
             if (target == null) throw new ArgumentNullException(nameof(target));
 
-            return m_serializer.Serialize(target);
+            return DefaultSerializer.Serialize(target);
         }
 
         public static object FromYaml(string text, Type type)
@@ -35,14 +38,14 @@ namespace UGF.Yaml.Runtime
             if (string.IsNullOrEmpty(text)) throw new ArgumentException("Value cannot be null or empty.", nameof(text));
             if (type == null) throw new ArgumentNullException(nameof(type));
 
-            return m_deserializer.Deserialize(text, type);
+            return DefaultDeserializer.Deserialize(text, type);
         }
 
         public static T FromYaml<T>(string text)
         {
             if (string.IsNullOrEmpty(text)) throw new ArgumentException("Value cannot be null or empty.", nameof(text));
 
-            return m_deserializer.Deserialize<T>(text);
+            return DefaultDeserializer.Deserialize<T>(text);
         }
     }
 }
